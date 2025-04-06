a52acd439f4be31804cb3f611950ee4cbc6f5cda
sweetykr7 <sweetykr@naver.com>
2025-04-06 21:58:10 +0900

    feat(gradle): add packaging setups

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "me.nettee"
-version = "0.1.0-rc1"
+version = "0.1.0-rc2"
 
 repositories {
     mavenCentral()
@@ -20,4 +21,35 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+// [2] Add task script to publish
+publishing {
+    publications {
+        //
+        create<MavenPublication>("cors-core") {
+            from(components["java"])
+            groupId = "com.github.merge-simpson"
+            artifactId = project.name
+            version = project.version.toString()
+        }
+    }
+}
+
+tasks.publishToMavenLocal.configure {
+    // assemble 작업 후 publishToMavenLocal 실행
+    //  assemble: 프로젝트의 아티팩트를 생성합니다. (jar 파일 등)
+    //  publishToMavenLocal: 빌드된 아티팩트를 로컬 저장소에 저장합니다. (.m2/repository)
+    dependsOn("assemble")
+}
+
+//tasks.bootJar {
+//    enabled = false
+//}
+
+tasks.jar {
+    enabled = true
+    archiveClassifier.set("") // remove suffix "-plain"
+    // 다운로드 하려는 파일 이름: example-error-code-api-0.1.0.jar
+    // 위 명령 누락 때 파일 이름: example-error-code-api-0.1.0-plain.jar
 }
